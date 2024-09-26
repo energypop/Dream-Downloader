3aedcfb8e823f87bd8aee1d009a70f812475db6c
Dreampop <sashareconquista@proton.me>
2024-09-26 17:54:52 +0200

    Iniciando la construccion del backend. Bootstrap instalado para un frontend responsive.

---
+++ middleware.js
@@ -0,0 +1,5 @@
+// Send link to the backend to process 
+
+document.getElementById("submitBtn").addEventListener("click", function(){
+    const target_url = document.getElementById("url").value; 
+});
